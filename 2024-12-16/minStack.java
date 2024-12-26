c830c0b0b0e434e42b75d84660ecc956f84d1bc8
Emily <emily.zy.lin@gmail.com>
2024-12-26 12:07:05 -0600

    corrected lc155 with O(1)

--- 2024-12-16/minStack.java
+++ 2024-12-16/minStack.java
@@ -1,5 +1,9 @@
 /*
 goal: constant time
+1) we need a new stack minStack to track the min value dynamically
+2) based on the above, we can retrieve the min value with O(1) time complexity. Otherwise it's O(n)
+3) no need to check if the stack is empty, the problem has clarified its a non-empty stack when calling those methods
+4) Leetcode No.4 subimission - solved directly without setting the Integer min
 
 */
 
@@ -8,57 +12,56 @@ import java.util.Stack;
 public class minStack {
 
     Stack<Integer> stack;
+    Stack<Integer> minStack;
 
     int min;
 
+    //constructor
     public minStack() {
         stack = new Stack<>();
-               
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE; //set the variable of min in the class    
     }
     
     public void push(int val) {
         stack.push(val);
+
+        if (val <= min) {
+            minStack.push(val);
+            min = val;
+        // } else {
+        //     minStack.push(min);
+        }
         
     }
     
     //remove the top element of the stack
     public void pop() {
-        if (!stack.isEmpty()) {
-            stack.pop();
-        } else {
-            return;
-        }
+       int poppedValue = stack.pop();
+
+       if (poppedValue == min) {
+            minStack.pop();
+
+            if (!minStack.isEmpty()) {
+                min = minStack.peek();
+            } else {
+                min = Integer.MAX_VALUE;
+            }
+       }
    
     }
     
     //gets the top element of the stack
     public int top() {
-        int top;
-
-        if (!stack.isEmpty()) {
-            top = stack.peek();
-        } else {
-            return null; //how to return 
-        }
-        
+        int topElement = stack.peek();
+        return topElement; 
     }
     
+    //retrieves the minimum element in the stack
     public int getMin() {
-        min = Integer.MAX_VALUE;
-        int topNum = stack.top();
-        int total = stack.size();
-
-        if (stack.isEmpty()) return null; //how to return if stack is empty
-
-        while (!stack.isEmpty()) {
-            min = Math.min(min, topNum);
-            total--;
-        }
 
         return min;
         
     }
 
-
-    
 }
