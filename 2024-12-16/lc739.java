84ef532bd806419a16db6246497d06e6573910a3
Emily <emily.zy.lin@gmail.com>
2024-12-27 11:58:45 -0600

    complete lc739

---
+++ 2024-12-16/lc739.java
@@ -0,0 +1,31 @@
+/*
+ * int[] temperatures <= daily temp
+ * goal: return int[] answer, answer[i] = the nearest higher number - current num, if none, answer[i] == 0;
+ * solution: two pointers
+ * 1, counting from the end of the array, if the previous index smaller, return the different; if bigger, 
+ */
+
+
+public class lc739 {
+     public int[] dailyTemperatures(int[] temperatures) {
+        //iterate and add the number to stack,
+        Stack<Integer> stack = new Stack<>();
+
+        int[] answer = new int[temperatures.length];
+        // no need to define the last index, the initial value is 0 for each index
+
+        for (int i = 0; i < temperatures.length; i++) {
+
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prev = stack.pop();
+                answer[prev] = i - prev;
+            } 
+            stack.push(i);
+
+        }
+        
+        return answer;
+    }
+            
+    
+}
