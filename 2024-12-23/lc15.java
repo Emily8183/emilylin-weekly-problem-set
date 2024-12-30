b4b95893169e34b9ea8871d695b558bd2eaa93ff
Emily <emily.zy.lin@gmail.com>
2024-12-30 15:00:37 -0600

    finish lc15 w two pointers

--- 2024-12-23/lc15.java
+++ 2024-12-23/lc15.java
@@ -5,43 +5,43 @@
  * key points: how to make sure no duplicated a, b, or c
  */
 
-public class lc15 {
+ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<Integer> list = new LinkedList<>();
-        List<List<Integer>> result = new ArrayList<>();
-
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if ( sum > 0) right--;
-                else if (sum < 0) left++;
-                else {
-                    result.add()
-                }   //add to the list
-
-
-            //skip the duplicated left and right
-
-            }
-
-
-            
-
-
-            
-
-        }
-
+       
+          List<List<Integer>> result = new ArrayList<>();
+  
+          Arrays.sort(nums);
+  
+          for (int i = 0; i < nums.length-2; i++) {
+              if (i > 0 && nums[i] == nums[i-1]) continue;//remove the duplicates of i
+  
+              int left = i + 1;
+              int right = nums.length - 1;
+  
+              while (left < right) {
+  
+                  int sum = nums[i] + nums[left] + nums[right];
+  
+                  if (sum > 0) right--;
+                  else if (sum < 0) left++;
+                  else {
+                      result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    //add to the list
+  
+                  //skip the duplicated left and right
+                  while (left < right && nums[left] == nums[left+1]) left++;
+                  while (left < right && nums[right] == nums[right-1]) right--;
+  
+                  left++;
+                  right--;
+                  }
+          
+              }
+      
+  
+          }
+          return result;
     }
-
-    
-}
+  
+      
+  }
